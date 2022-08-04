f03051dfdbf0c489ce91ff6e6489147db13dae24
randi sihaloho <randinezer24@gmail.com>
2022-08-04 19:30:59 +0700

    done

---
+++ src/test/java/com/siloam/plugin/LoginStep.java
@@ -0,0 +1,30 @@
+package com.siloam.plugin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.siloam.plugin.DriverSet;
+
+public class LoginStep {
+	
+	private WebDriver driver;
+	
+	
+//	@FindBy (xpath = "//input[@name=\"username\"]")
+//	private WebElement formUsername;
+//	
+//	@FindBy (xpath = "//input[@name=\"password\"]")
+//	private WebElement formPassword;
+//	
+//	@FindBy (xpath = "//button[@type=\"submit\"]")
+//	private WebElement btnLogin;
+	
+	public void loginSales() {
+		driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("D6200927");
+		driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("19971023");
+		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		
+	}
+}
---
+++ src/test/resources/features/UploadDocument.feature
@@ -0,0 +1,3 @@
+Feature: Upload Dokumen Testing
+Scenario: Akses menu upload document
+	When User sales akses menu upload 
